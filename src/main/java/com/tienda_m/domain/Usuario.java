082eb7f7fc9a68a37e0dbb531ac48eef6b5aec5f
Andre56733 <159095395+Andre56733@users.noreply.github.com>
2025-11-19 20:04:44 -0600

    Semana 11

--- src/main/java/com/tienda_m/domain/Usuario.java
+++ src/main/java/com/tienda_m/domain/Usuario.java
@@ -2,13 +2,15 @@ package com.tienda_m.domain;
 
 import jakarta.persistence.*;
 import java.io.Serializable;
+import java.util.HashSet;
 import java.util.Set;
 import lombok.Data;
 
 @Data
 @Entity
 @Table(name = "usuario")
-public class Usuario implements Serializable {    
+public class Usuario implements Serializable {
+
     private static final long serialVersionUID = 1L;
 
     @Id
@@ -21,15 +23,15 @@ public class Usuario implements Serializable {
     private String apellidos;
     private String correo;
     private String telefono;
-    private String rutaImagen;    
+    private String rutaImagen;
     private boolean activo;
 
     // Relación Many-to-Many con la entidad Rol
-@ManyToMany(fetch = FetchType.LAZY) 
-@JoinTable(
-    name = "usuario_rol",
-    joinColumns = @JoinColumn(name = "id_usuario"),
-    inverseJoinColumns = @JoinColumn(name = "id_rol")
-)
-private Set<Rol> roles;
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable(
+            name = "usuario_rol",
+            joinColumns = @JoinColumn(name = "id_usuario"),
+            inverseJoinColumns = @JoinColumn(name = "id_rol")
+    )
+    private Set<Rol> roles = new HashSet<> ();
 }
---
+++ src/main/java/com/tienda_m/repository/RolRepository.java
@@ -0,0 +1,11 @@
+package com.tienda_m.repository;
+
+import com.tienda_m.domain.Rol;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RolRepository extends JpaRepository<Rol, Integer> {
+
+    public Optional<Rol> findByRol(String rol);
+
+}
